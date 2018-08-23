087acd2b4fc31a911087e5b4f5c6a6236d0d98be
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-23 11:08:59 +0200

    change in actions.js

--- actions.js
+++ actions.js
@@ -17,7 +17,7 @@ dispatch(addComment('nowy komentarz!'));
 function removeComment(id) {
     return {
     	type: REMOVE_COMMENT,
-        id:
+        id
     }
 }
 
@@ -27,7 +27,7 @@ function editComment(text,id) {
     return {
         type: EDIT_COMMENT,
         text,
-        id:
+        id
     }
 }
 
@@ -36,8 +36,7 @@ dispatch(editComment('edytuje komentarz',3));
 function thumbUp(id) {
     return {
         type:THUMB_UP_COMMENT,
-        id:,
-        votes:
+        id
     }
 }
 
@@ -46,8 +45,7 @@ dispatch(thumbUp(3));
 function thumbDown(id) {
     return {
         type: THUMB_DOWN_COMMENT,
-        id:,
-        votes:
+        id
     }
 }
 
